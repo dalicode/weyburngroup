ada6c1ee46c1a2eec626b4d123e6eb8bb42a7913
Damon <damonliwork@hotmail.com>
2020-02-07 12:13:58 -0500

    adds active styling to navigation bar

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import classes from "./Navigation.module.css";
 import logo from "../../assets/logo.svg"
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 class navigation extends Component {
   state = {
@@ -16,23 +16,23 @@ class navigation extends Component {
   return (
       <div id="navMenu" className={classes.navMenu}>
         <div className = {classes.logodiv}> 
-          <Link to={'/'}>
+          <NavLink to={'/'}>
             <img id="logo" className={classes.logo} src={logo} alt="logo"/> 
-          </Link>
+          </NavLink>
         </div>
         <div id="iconBar" className={classes.iconBar} onClick={this.show}>
           {/* <img src={menu} alt="menu"/> */}
           <p>MENU</p>
         </div>
         <ul id="ul" className={this.state.active ? `${classes.ul} ${classes.responsive}` : classes.ul} onClick={this.show}>
-          <li><Link className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/'}> Home </Link></li>
-          <li><Link className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={{
+          <li><NavLink exact={true} activeClassName=      {classes.ActiveNavLink} className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/'}> Home </NavLink></li>
+          <li><NavLink activeClassName={classes.ActiveNavLink} className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={{
           pathname: '/gallery',
           state: {
             active: 'renovation'
-          }}}> Gallery </Link></li>
-          <li><Link className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/process'}> Services </Link></li>
-          <li><Link className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/contact'}> Contact </Link></li>
+          }}}> Gallery </NavLink></li>
+          <li><NavLink activeClassName={classes.ActiveNavLink} className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/process'}> Services </NavLink></li>
+          <li><NavLink activeClassName={classes.ActiveNavLink} className={this.state.active ? `${classes.NavLink} ${classes.responsive}` : classes.NavLink} to={'/contact'}> Contact </NavLink></li>
         </ul>
       </div>
       
