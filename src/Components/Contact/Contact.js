a3ea3d26c6f58cc357176e10bc38d217b4e9a4fb
Damon <damonliwork@hotmail.com>
2020-02-10 20:06:32 -0500

    adds formspree contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,11 +2,24 @@ import React from "react";
 import classes from "./Contact.module.css";
 
 const contact = () => {
-  return (
-      <div className={classes.Contact}>
-        <p> Contact Page </p>
-      </div>
-  );
+    return (
+        <div className={classes.Contact}>
+            <div className={classes.LeftDiv}> Left Div </div>
+            <div className={classes.RightDiv}>
+                <h3> EMAIL US NOW </h3>
+                <form action="https://formspree.io/myywbdqz" method="POST">
+                    <p>Full Name:</p>
+                    <input type="text" name="name" />
+                    <p>Email Address:</p>
+                    <input type="text" name="_replyto" />
+                    <p>Message:</p>
+                    <textarea name="message"></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default contact;
