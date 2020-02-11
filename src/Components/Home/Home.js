5e1fea5dd1e84b5bbdb2da59c16a89aef35bc58c
Damon Li <damon@Damons-MacBook-Pro.local>
2020-02-11 15:05:32 -0500

    fixes mobile spacing of content pages from the top as well as fixes gallery page mobile gallery button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,6 @@ const home = () => {
   return (
       <div className={classes.Home}>
         <div className={classes.LeftHeader}><p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi incidunt rem, quos magni, dolore quod qui porro, dolores aspernatur officiis soluta! Tenetur est facere incidunt impedit iste, nesciunt iure accusamus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid necessitatibus dolor fugiat consequuntur fugit illo! Perferendis, fugit voluptas non illo ea nobis sapiente maxime culpa eum rem dolorum quae ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aperiam et dicta, totam aut ad rerum velit minus provident magni saepe blanditiis vitae sint quibusdam, reiciendis ullam voluptatum ratione. Cumque?</p> </div>
-        <SubHeading> Gallery </SubHeading>
         <Gallery/>
       </div>
   );
