495e6e37c912e0e5fc2c18d4e847d182a7191047
Damon <damonliwork@hotmail.com>
2020-02-12 21:05:42 -0500

    replaces home placeholder text and fixes viewport height issue with home banner

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,7 @@ const home = () => {
       <div className={classes.Home}>
         <div className={classes.LeftHeader}>
         <h1>Construction company based in Toronto, Ontario </h1>
-        <p> "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi incidunt rem, quos magni, dolore quod qui porro, dolores aspernatur officiis soluta! Tenetur est facere incidunt impedit iste, nesciunt iure accusamus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid necessitatibus dolor fugiat consequuntur fugit illo! Perferendis, fugit voluptas non illo ea nobis sapiente maxime culpa eum rem dolorum quae ducimus?" </p> 
+        <p> Hi! We are Dave and Jack - two driven individuals who want to help you achieve your dream home. Home remodelling is our passion and is what drives Weyburn Group forward in today's constantly evolving industry.  We build to improve and revitalize people's living spaces and their local communities. With over 10+ years of combined construction experience, our team is ready to transform your home. Weyburn Group places a strong focus on communication, quality, and safety. We're here to help create beautiful spaces together with you. We offer a variety of home improvement services in both interior and exterior construction. </p> 
         <button className={classes.Button}> <NavLink className={classes.NavLink} to={'/contact'}> Contact Us </NavLink></button>
         </div>
         <Gallery/>
