53280f3bd9cdd0bc0b793e966a18cb76150e917f
Damon Li <damon@Damons-MacBook-Pro.local>
2020-02-13 14:51:35 -0500

    makes services look better by aligning pictures correctly

--- src/Components/Process/ServiceChunk/ServiceChunk.js
+++ src/Components/Process/ServiceChunk/ServiceChunk.js
@@ -3,11 +3,11 @@ import classes from "./ServiceChunk.module.css"
 
 const serviceChunk = props => {
   return (
-    <div style={props.reverse ? {flexDirection:'row-reverse'} : null}className={classes.ServiceChunk}>
+    <div style={props.reverse ? {flexDirection:'row-reverse'} : null} className={classes.ServiceChunk}>
       <div className={classes.LeftDiv}>
         <p>{props.children}</p>
       </div>
-      <div className={classes.RightDiv}>
+      <div style={props.reverse ? {justifyContent:'flex-start'} : null} className={classes.RightDiv}>
       <div className={classes.ImageWrapper}>
         <img src={props.src} alt={props.children[0]}/>
         </div>
