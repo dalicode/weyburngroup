44241965adfe03d14ab41e071d26312564b3163d
Damon <damonliwork@hotmail.com>
2020-02-13 22:21:33 -0500

    made contact page cooler

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -27,23 +27,27 @@ const contact = () => {
           <form action="https://formspree.io/myywbdqz" method="POST">
             <p>Full Name</p>
             <input placeholder="John Smith" type="text" name="name" />
+            <span className={classes.focusBg}></span>
             <p>Email Address</p>
             <input
               placeholder="johnsmith@abc.com"
               type="text"
               name="_replyto"
             />
-            <p>Phone Number</p>
+            <p>Phone Number*</p>
             <NumberFormat
               placeholder="(123)-456-7890"
               format="(###) ###-####"
               mask="_"
+              name="phone"
+              required
             />
-            <p>Message</p>
+            <p>Message*</p>
             <textarea
               rows="4"
               placeholder="Start typing message here..."
               name="message"
+              required
             ></textarea>
             <button type="submit">Submit</button>
           </form>
